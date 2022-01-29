4c77b2dbd255263249977c6d74909ce5eeb86426
Mosharaf456 <mosharaf111hossain@gmail.com>
2022-01-30 01:25:53 +0600

    16_image_upload into server(local machine) laravel 8

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function create()
+    {
+        return view('fileup');
+    }
+    public function store()
+    {
+        request()->validate([
+            'user_img' => 'required | image | max:1000 | min:10'
+            // 'user_img' => 'required | mimes:png,jpeg,doc,docx,pdf,jpg,gif'
+            // 'user_img' => 'required | mimes:png'
+        ]);
+
+        if (request()->hasFile('user_img'))  //if file available
+        {
+            $destinationPath = 'uploads'; // this path is public/uploads  
+
+            $extension = request()->file('user_img')->getClientOriginalExtension(); //
+            // $file_name = rand(). "." . $extension;
+            $file_name = uniqid(). "." . $extension;
+            request()->file('user_img')->move($destinationPath, $file_name);
+
+            return back();
+        }
+    }
+
+}
